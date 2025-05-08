2ee2f3ce05a3b0b98e23602e15055d8cf093352c
Mqlvin <pricklydotcactus@gmail.com>
2025-05-08 10:10:15 +0100

    restructure backend code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,13 +10,12 @@ use bbcore::drawing::DrawMethod;
 use bbcore::preview::generate_preview;
 use bbcore::instruction::InstructionSet;
 use std::fs::File;
-use std::io::{BufReader, Read, Write};
-use std::ops::DerefMut;
+use std::io::Write;
 use tokio::sync::Mutex;
-use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use std::sync::Arc;
-use tauri::{Emitter, Manager, State};
-use bbcore::client::state::ClientState;
+
+pub mod file;
+pub mod client;
 
 #[tauri::command(async)]
 fn gen_preview(app: tauri::AppHandle, style_id: &str, json_params: &str) -> String {
@@ -116,134 +115,22 @@ fn gen_preview(app: tauri::AppHandle, style_id: &str, json_params: &str) -> Stri
 
 
 
-#[tauri::command(async)]
-async fn send_to_firmware(app: tauri::AppHandle, state: State<'_, AppState>) -> Result<String, String> {
-
-    let win = app.get_webview_window("main").unwrap();
-
-    let cache_dir = tauri::Manager::path(&app).app_cache_dir().expect("Should get cache dir");
-    let _ = std::fs::create_dir_all(&cache_dir).map_err(|s| s.to_string());
-    let ins_file_path = cache_dir.join("instructions.bin");
-    let mut ins_file = File::open(ins_file_path).unwrap();
-    let mut buffer = Vec::new();
-    let _ = ins_file.read_to_end(&mut buffer).unwrap();
-
-    let ins_set = match InstructionSet::new(buffer, 0., 0.) { 
-        Ok(val) => { val },
-        Err(e) => { println!("{e}"); return Err(e.to_string()); },
-    };
-
-    let mut buf_idx_lock = state.buf_idx.lock().await;
-    *buf_idx_lock = 0;
-    drop(buf_idx_lock);
-
-
-    win.emit("firm-prog", format!(r#"{{"event":"populate_network", "address":"{}"}}"#, "192.168.0.16:8180")).unwrap();
-    win.emit("firm-prog", format!(r#"{{"event":"populate_draw", "totalBytes":"{}"}}"#, ins_set.get_binary().len())).unwrap();
-
-    // create new socket, and split it into owned directions
-    let (client, machine_config) = ClientState::new("192.168.0.16", 8180).await.unwrap();
-    let (stream_reader, stream_writer) = client.into_split();
-
-    win.emit("firm-prog", r#"{"event":"connection", "message":"Machine accepted connection"}"#).unwrap();
-    win.emit("firm-prog", format!(r#"{{"event":"populate_machine", "insBytes":"{}", "stepSpeed":"{}", "pulseWidth":"{}", "protocol":"{}"}}"#, machine_config.instruction_buffer_size, machine_config.max_motor_speed, machine_config.min_pulse_width, machine_config.protocol_version)).unwrap();
-    
-    // lock writer, set writer as owned, drop it
-    let mut writer_lock = state.writer.lock().await;
-    *writer_lock = Some(stream_writer);
-    drop(writer_lock);
-
-    // lock reader, set writer as owned, launch listen task
-    let mut reader_lock = state.reader.lock().await;
-    *reader_lock = Some(stream_reader);
-    let reader = reader_lock.as_mut().unwrap();
-
-    ClientState::listen(reader, &state.writer, &state.buf_idx, &ins_set, &machine_config, move |msg| { let _ = win.emit("firm-prog", msg.as_str()); }).await;
-
-    let mut writer_lock = state.writer.lock().await;
-    *writer_lock = None;
-    *reader_lock = None;
-    let mut paused_lock = state.paused_flag.lock().await;
-    *paused_lock = false;
-    let mut buf_idx_lock = state.buf_idx.lock().await;
-    *buf_idx_lock = 0;
-
-    drop(paused_lock);
-    drop(writer_lock);
-    drop(reader_lock);
-    drop(buf_idx_lock);
-
-    println!("Cleanly exited drawing.");
-
-    Ok("".to_owned())
-}
-
-#[tauri::command(async)]
-async fn pause_firmware(app: tauri::AppHandle, state: State<'_, AppState>) -> Result<String, String>  {
-
-    let mut writer_lock = state.writer.lock().await;
-    let writer = writer_lock.as_mut().unwrap();
-
-    let mut paused_lock = state.paused_flag.lock().await;
-    *paused_lock.deref_mut() = !(*paused_lock);
-
-    let win = app.get_webview_window("main").unwrap();
-
-    ClientState::pause(writer, *paused_lock, move |msg| { let _ = win.emit("firm-prog", msg.as_str()); }).await;
-    
-    // drops occur out of scope
-
-    Ok("".to_owned())
-}
-
-#[tauri::command(async)]
-async fn move_pen_to_start(app: tauri::AppHandle, state: State<'_, AppState>) -> Result<String, String>  {
-
-    let cache_dir = tauri::Manager::path(&app).app_cache_dir().expect("Should get cache dir");
-    let start_file_path = cache_dir.join("start.bin");
-    let start_file = File::open(start_file_path).unwrap();
-    let mut start_contents = String::new();
-    BufReader::new(start_file).read_to_string(&mut start_contents).unwrap();
-    let start_pos: Vec<f64> = start_contents.split_whitespace().filter_map(|s| s.parse::<f64>().ok()).collect();
-    let phys_dim = PhysicalDimensions::new(754., (754. - 210.) / 1.98, 192., 210., 297.);
-    if let Err(str) = bbcore::client::move_to_start("192.168.0.16", 8180, &phys_dim, start_pos[0], start_pos[1]) {
-        return Err(str.to_string().to_owned());
-    }
-
-    Ok("".to_owned())
-}
-
-#[tauri::command(async)]
-async fn stop_drawing(app: tauri::AppHandle, state: State<'_, AppState>) -> Result<String, String>  {
-    
-    let win = app.get_webview_window("main").unwrap();
-
-    let mut writer_lock = state.writer.lock().await;
-    let writer = writer_lock.as_mut().unwrap();
-    
-    ClientState::stop(writer, move |msg| { let _ = win.emit("firm-prog", msg.as_str()); }).await;
-
-    // will finish ClientState::listen in other thread
-
-    Ok("".to_owned())
-
-}
-
-pub struct AppState {
-    pub writer: Arc<Mutex<Option<OwnedWriteHalf>>>,
-    pub reader: Arc<Mutex<Option<OwnedReadHalf>>>,
-    pub paused_flag: Arc<Mutex<bool>>,
-    pub buf_idx: Arc<Mutex<usize>>,
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let state = AppState { writer: Arc::new(Mutex::new(None)), reader: Arc::new(Mutex::new(None)), paused_flag: Arc::new(Mutex::new(false)), buf_idx: Arc::new(Mutex::new(0)) };
+    let state = client::AppState { writer: Arc::new(Mutex::new(None)), reader: Arc::new(Mutex::new(None)), paused_flag: Arc::new(Mutex::new(false)), buf_idx: Arc::new(Mutex::new(0)) };
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .manage(state)
-        .invoke_handler(tauri::generate_handler![gen_preview, send_to_firmware, pause_firmware, move_pen_to_start, stop_drawing])
+        .invoke_handler(tauri::generate_handler![
+            gen_preview,
+            client::send_to_firmware,
+            client::pause_firmware,
+            client::move_pen_to_start,
+            client::stop_drawing,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
